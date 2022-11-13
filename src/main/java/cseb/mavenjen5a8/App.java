e59a2e008fa3bf065db119fab7b5c28b977e3e75
Mamatha <Mamatha@LAPTOP-7J9ULA4Q>
2022-11-13 23:19:51 +0530

    hello

---
+++ src/main/java/cseb/mavenjen5a8/App.java
@@ -0,0 +1,13 @@
+package cseb.mavenjen5a8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO JENKINS !" );
+    }
+}
